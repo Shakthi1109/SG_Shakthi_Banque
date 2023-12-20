f2e7bc85f8752be757c085f3c5a9fc37ad11247c
Shakthi Vel <mail2shakthivel98@gmail.com>
2023-12-20 01:26:59 +0100

    Deposit done

--- src.py
+++ src.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 def instantiateCustomerDb():
     with open('customerDB.csv', 'w') as customerDB:
-        customerDB.write("Surname,Name,Email,Password,Balance\n")
+        customerDB.write("Surname,Name,Email,Password,Transactions")
         customerDB.close()
 
 def checkCustomerStatus():
@@ -18,7 +20,6 @@ def checkCustomerStatus():
 
 def createAccount():
     print("Let's create an account for you")
-    balance = 0
     print("Enter your Surname")
     surname = input()
     print("Enter your name")
@@ -31,7 +32,9 @@ def createAccount():
     confirmPassword = input()
     if password == confirmPassword:
         print("Account created successfully")
-        writeToCustomerDB(surname, name, email, password, balance)
+        with open('customerDB.csv', 'a') as customerDB:
+            customerDB.write("\n"+surname + "," + name + "," + email + "," + password + "," + "Deposit - "+ str(datetime.now().strftime("%d/%m/%Y"))+ " - "+ str(0) + " - " + str(0))
+        customerDB.close()
         checkCustomerStatus()
     else:
         print("Password mismatch")
@@ -42,32 +45,37 @@ def login():
     email = input()
     print("Enter your password")
     password = input()
+    loginFlag = False
     try:
-        with open('customerDB.csv', 'r') as customerDB:
-            for line in customerDB:
+
+        # Read the file content into memory
+        with open('customerDB.csv', 'r') as file:
+            lines = file.readlines()
+            
+            for line in lines:
                 if email in line:
                     parts = line.split(',')
-                    if password in parts[3]:
-                        print("Login successful")
-                        homePage(parts[1], parts[0], parts[2])
-                    else:
-                        print("Invalid password")
-                        login()
-                else:
-                    print("Email not found. Please create an account or contact customer support")
-                    createAccount()
+                
+                    if email == parts[2]:
+                        if password == parts[3]:
+                            print("Login successful")
+                            loginFlag = True
+                            homePage(parts[1], parts[0], parts[2])
+                        else:
+                            print("Invalid password")
+                            login()
+            if not loginFlag:
+                print("Email not found. Please create an account or contact customer support")
+                    
+        file.close()
+
     except FileNotFoundError:
         print("The file 'customerDB.csv' was not found.")
     except PermissionError:
         print("You don't have permission to read the file 'customerDB.csv'.")
     except Exception as e:
         print("An error occurred:", e)
-
-
-def writeToCustomerDB(surname, name, email, password, balance):
-    with open('customerDB.csv', 'a') as customerDB:
-        customerDB.write(surname + "," + name + "," + email + "," + password + "," + str(balance) + "\n")
-        customerDB.close()
+  
 
 def homePage(name, surname, email):
     print("Welcome "+ name +" "+ surname)
@@ -78,7 +86,7 @@ def homePage(name, surname, email):
     print("4. Exit")
     option = input()
     if option == '1':
-        deposit(email)
+        deposit(name, surname, email)
     elif option == '2':
         withdraw()
     elif option == '3':
@@ -90,9 +98,8 @@ def homePage(name, surname, email):
         homePage()
 
 
-#TO DO DESPOIT
-                
-def deposit(email):
+             
+def deposit(name, surname, email):
     print("Enter the amount you would like to deposit")
     amount = input()
     
@@ -101,23 +108,32 @@ def deposit(email):
         lines = file.readlines()
 
     # Iterate through the lines and update the balance if email is found
-    found = False
-    with open('customerDB.csv', 'w') as customerDB:
+    with open('customerDB.csv', 'a') as customerDB:
         for line in lines:
             if email in line:
                 parts = line.split(',')
-                balance = int(parts[4].strip())
-                newBalance = balance + int(amount)
-                parts[4] = str(newBalance) + "\n"
-                updated_line = ','.join(parts)
-                customerDB.write(updated_line)
-                found = True
-                print("Deposit successful")
-            else:
-                customerDB.write(line)
+                if email == parts[2]:
+                    lastTransaction = parts[-1].strip()
+                    balance = float(lastTransaction.split('-')[3].strip())
+                    newBalance = balance + float(amount)
+                    customerDB.write(", Deposit - "+ str(datetime.now().strftime("%d/%m/%Y"))+ " - "+ str(amount) + " - " + str(newBalance))
+                    print("Deposit successful")
+    
+    customerDB.close()
+    anotherTransaction(name, surname, email)
 
-        if not found:
-            print("Email not found")
+
+def anotherTransaction(name, surname, email):
+    print("Would you like to do another transaction ? (y/n)")                
+    transactionFlag = input()
+    if transactionFlag == 'y':
+        homePage(name, surname, email)
+    elif transactionFlag == 'n':
+        print("Thank you for banking with us. Have a nice day")
+        exit()
+    else:
+        print("Invalid input")
+        anotherTransaction(name, surname, email)
 
 
 if __name__ == "__main__":
